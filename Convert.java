345f0b90f364ae49ea7bd675f28cbe64ed43d3d5
Sourabhpal1998 <55873829+Sourabhpal1998@users.noreply.github.com>
2022-03-16 10:47:37 +0530

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,27 @@
+public class Convert {
+
+    public static void convert(int num) {
+
+        System.out.println("Integer: " + num);
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanLiterals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder roman = new StringBuilder();
+
+        for(int i=0;i<values.length;i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                roman.append(romanLiterals[i]);
+            }
+        }
+        System.out.println("Roman: " + roman.toString());
+        System.out.println("---------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        convert(25);
+        convert(36);
+        convert(542);
+
+    }
+}
---
+++ Swap.java
@@ -0,0 +1,40 @@
+public class Swap {
+
+    public static void main(String[] args) {
+        String str = "TRUE";
+        int n = str.length();
+        Swap swap = new Swap();
+        swap.permute(str, 0, n-1);
+    }
+
+
+    private void permute(String str, int l, int r)
+    {
+        if (l == r)
+            System.out.println(str);
+        else
+        {
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str,l,i);
+                permute(str, l+1, r);
+                str = swap(str,l,i);
+            }
+        }
+    }
+
+
+    public String swap(String a, int i, int j)
+    {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i] ;
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+    }
+
+
+
+
